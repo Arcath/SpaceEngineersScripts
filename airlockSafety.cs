9806d7a3afb64db6bdbf9c457288e05756859ca0
Adam Laycock <adam@arcath.net>
2015-04-08 21:17:31 +0100

    Airlock Safety Script

---
+++ airlockSafety.cs
@@ -0,0 +1,61 @@
+/*
+
+Pressure Safe Airlocks Written by Adam Laycock
+Run once per second with a timer block to ensure that all your airlocks stay safe
+
+*/
+public static string[][] AIRLOCKS =
+{
+  new string[]
+  {
+    "HA Outer Door",
+    "HA Inner Door",
+    "AV Hangar Air Lock"
+  },
+  new string[]
+  {
+    "LA Outer Door",
+    "LA Inner Door",
+    "AV Left Airlock"
+  },
+  new string[]
+  {
+    "RA Outer Door",
+    "RA Inner Door",
+    "AV Right Airlock"
+  }
+};
+
+void handleAirlock(string[] airlock) {
+  IMyDoor outerDoor = (IMyDoor)GridTerminalSystem.GetBlockWithName(airlock[0]);
+  IMyDoor innerDoor = (IMyDoor)GridTerminalSystem.GetBlockWithName(airlock[1]);
+  IMyAirVent airVent = (IMyAirVent)GridTerminalSystem.GetBlockWithName(airlock[2]);
+
+  bool oxygenStatus = (airVent.IsPressurized() && airVent.GetOxygenLevel() != 0);
+
+  if(oxygenStatus)
+  {
+    innerDoor.GetActionWithName("OnOff_On").Apply(innerDoor);
+    outerDoor.GetActionWithName("OnOff_Off").Apply(outerDoor);
+  }
+  else
+  {
+    if(innerDoor.Open)
+    {
+      innerDoor.GetActionWithName("Open_Off").Apply(innerDoor);
+    }
+    else
+    {
+      innerDoor.GetActionWithName("OnOff_Off").Apply(innerDoor);
+    }
+    outerDoor.GetActionWithName("OnOff_On").Apply(outerDoor);
+  }
+}
+
+void Main()
+{
+  for (int i = 0; i < AIRLOCKS.Length; i++)
+  {
+    handleAirlock(AIRLOCKS[i]);
+  }
+}
